ab7476bba9f23f0634d322ded53190e39c7f1479
AnastasiaKampantai <s253724@dtu.dk>
2025-11-05 11:51:04 +0000

    Final ui and app

--- ui.R
+++ ui.R
@@ -3,81 +3,75 @@ library("bslib")
 
 
 ui <- page_fluid(
+  theme = bs_theme(
+    version = 5,
+    font_scale = 1.1,
+    base_font = font_google("Poppins"),
+    heading_font = font_google("Fredoka One"),
+    "card-border-radius" = "1.5rem",
+    "body-bg" = "#EEF7DC"
+  ),
+  
   layout_columns(
     col_widths = 12,
-    card(
-      titlePanel("Virtual Central Dogma"),
-      style = "background-color: #f0f0f0; padding: 15px;"
+    card(titlePanel("Virtual Central Dogma")
     )),
+  
   layout_columns(
     col_widths = 12,
-    card(
-      titlePanel("About"),
-      helpText("This app allows us to follow the central dogma of molecular biology. It generates the flow of genetic information from DNA to RNA to protein, a process involving transcription (DNA to RNA) and translation (RNA to protein). Each of the unique codons are translated into different amino acids and predictions of unique protein properties can be made acording to the different amino acid quantities in a sequence.")
+    card(titlePanel("About"),
+      helpText("Welcome to the *Virtual Central Dogma*! This app simulates the flow of genetic information from DNA → RNA → Protein. 
+                Watch transcription and translation in action as you tweak probabilities of different bases and see how the resulting protein changes!")
     )),
+  
   layout_columns(
     col_widths = 12,
-    card(
-      card_header("Virtual Gene Generator"),
-      sliderInput(inputId = "n_bases",
-                  label = "Number of bases:",
-                  min = 1,
-                  max = 60,
+    card(card_header("Virtual Gene Generator"),
+      sliderInput("n_bases", "Number of bases:", 
+                  min = 1, 
+                  max = 60, 
                   value = 30,
                   width = "100%"),
       layout_columns(
         col_widths = c(3, 3, 3, 3),
-        numericInput(inputId = "prob_A",
-                     label = "Probability of A",
-                     value = 0.25,
-                     min = 0,
-                     max = 1,
-                     step = 0.1),
-        numericInput(inputId = "prob_T",
-                     label = "Probability of T",
-                     value = 0.25,
-                     min = 0,
-                     max = 1,
-                     step = 0.1),
-        numericInput(inputId = "prob_C",
-                     label = "Probability of C",
-                     value = 0.25,
-                     min = 0,
-                     max = 1,
-                     step = 0.1),
-        numericInput(inputId = "prob_G",
-                     label = "Probability of G",
-                     value = 0.25,
-                     min = 0,
-                     max = 1,
-                     step = 0.1)
+        numericInput("prob_A", "Probability of A", 
+                     value = 0.25, 
+                     min = 0, 
+                     max = 1),
+        numericInput("prob_T", "Probability of T", 
+                     value = 0.25, 
+                     min = 0, 
+                     max = 1),
+        numericInput("prob_C", "Probability of C", 
+                     value = 0.25, 
+                     min = 0, 
+                     max = 1),
+        numericInput("prob_G", "Probability of G", 
+                     value = 0.25, 
+                     min = 0, 
+                     max = 1)
       ))),
+  
   layout_columns(
     col_widths = 12,
-    card(
-      card_header("Virtual Gene output"),
-      mainPanel(
-        verbatimTextOutput(outputId = "dna")
-      ))),
+    card(card_header("DNA Output"), 
+         mainPanel(verbatimTextOutput("dna")))
+  ),
+  
   layout_columns(
     col_widths = 12,
-    card(
-      card_header("Virtual RNA output"),
-      mainPanel(
-        verbatimTextOutput(outputId = "rna")
-    ))),
+    card(card_header("RNA Output"), 
+         mainPanel(verbatimTextOutput("rna")))
+  ),
+  
   layout_columns(
     col_widths = 12,
-    card(
-      card_header("Virtual Protein output"),
-      mainPanel(
-        verbatimTextOutput(outputId = "protein")
-      ))),
+    card(card_header("Protein Output"), 
+         mainPanel(verbatimTextOutput("protein")))
+  ),
+  
   layout_columns(
-    col_widths = c(6,6),
-    card(
-      card_header("Virtual Base Frequencies output"),
-      mainPanel(
-        tableOutput(outputId = "freqs")
-      )))
-)
+    col_widths = c(6, 6),
+    card(card_header("Base Frequencies"), 
+         mainPanel(tableOutput("freqs")))
+  ))
